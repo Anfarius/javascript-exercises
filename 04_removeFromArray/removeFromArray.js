d499ec497b32b9998de6deb4e0c93769d345a29b
Kacper Kaczmarzyk <kacper.kaczmarzyk@gmail.com>
2021-12-24 00:09:24 +0100

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (arg of args) {
+        const index = array.indexOf(arg);
+        if (index === -1) continue;
+        array.splice(index, 1);
+    }
+    let outputArray = array;
+    return outputArray;
 };
 
 // Do not edit below this line
