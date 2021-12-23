816ac82fca75d8d2c29469e128f89573262920be
Kacper Kaczmarzyk <kacper.kaczmarzyk@gmail.com>
2021-12-23 20:31:02 +0100

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+const repeatString = function(stringToRepeat, number) {
+    let outputString = "";
+    if (number < 0) {
+        outputString = "ERROR";
+    } else {
+        for (let i = 0; i < number; i++) outputString += stringToRepeat;
+    }
+    return outputString;
+    }
 
 // Do not edit below this line
 module.exports = repeatString;
