acb2f026a9f61f31b139b489500ed879bbc92d28
Kacper Kaczmarzyk <kacper.kaczmarzyk@gmail.com>
2021-12-24 00:36:29 +0100

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let minNum;
+    let maxNum;
 
+    if (a < 0 || typeof(a) !== "number" || b < 0 || typeof(b) !== "number" ||
+            a === b) {
+        return 'ERROR';
+    }
+
+    if (a < b) {
+        minNum = a;
+        maxNum = b;
+    } else {
+        minNum = b;
+        maxNum = a;
+    }
+
+    let sum = 0;
+
+    for (minNum ; minNum <= maxNum ; minNum++) sum += minNum;
+
+    return sum;
 };
 
 // Do not edit below this line
