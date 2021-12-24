8e935350b7b54e5061cd5319c2c0cbc37dda7ce2
Kacper Kaczmarzyk <kacper.kaczmarzyk@gmail.com>
2021-12-24 01:14:19 +0100

    complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let tempC = (tempF - 32) * (5 / 9);
+  return parseFloat(tempC.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF = tempC * (9 / 5) + 32;
+  return parseFloat(tempF.toFixed(1));
 };
 
 // Do not edit below this line
